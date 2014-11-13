00d9b1e00b85a1c36ad8f805bea7f41b66570f9c
Daniel Platt <github@ofdan.co.uk>
2014-11-13 20:34:51 +0000

    Initial Commit

---
+++ Segue/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  Segue
+//
+//  Created by Daniel Platt on 13/11/2014.
+//  Copyright (c) 2014 Daniel Platt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ Segue/MasterViewController.h
@@ -0,0 +1,23 @@
+//
+//  MasterViewController.h
+//  Segue
+//
+//  Created by Daniel Platt on 13/11/2014.
+//  Copyright (c) 2014 Daniel Platt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
